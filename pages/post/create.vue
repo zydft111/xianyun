c8e76172a16d4190b687bb60efa0f9404429122c
qige <574857804@qq.com>
2020-02-20 18:56:17 +0800

    攻略发布页面布局

--- pages/post/create.vue
+++ pages/post/create.vue
@@ -40,7 +40,7 @@
       </div>
       <div class="aside">
         <div class="draft-box">
-          <h4>草稿箱(1)</h4>
+          <h4>草稿箱<span>(1)</span></h4>
         </div>
       </div>
     </el-row>
