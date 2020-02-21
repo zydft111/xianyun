4f4c9b6f857065a1f1157f921a209c8b205260bb
贡献者名字 <aaabbbccc@qq.com>
2020-02-22 02:08:05 +0800

    '重做侧边栏'

---
+++ components/post/postlist.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if='type===3'>
+       <h2><nuxt-link to='#'>{{item.title}}</nuxt-link></h2>
+       <p v-html="item.summary"></p>
+       <nuxt-link  v-for="(each,index) in item.images" :key="index" to='#'>
+         <img :src="each" alt=""></nuxt-link>
+         <div class="info">
+           <el-row>
+             <span><i class="el-icon-location-outline"></i>
+             {{item.cityName}}&nbsp;by&nbsp;
+             `${$axios.defaults.baseURL}${item.account.defaultAvatar}`
+             &nbsp;<i class="el-icon-view"></i>&nbsp;{{item.watch}}</span>
+           </el-row>
+         </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,170 +1,274 @@
 <template>
-  <el-container>
-  <el-aside width="350px">
-      <el-row>
-        <el-col :span="24">
-    <el-menu
-      :router='true'
-      :unique-opened="true"
-      default-active="1"
-      class="el-menu-vertical-demo"
-     
-      background-color="#545c64"
-      text-color="#fff"
-      active-text-color="#ffd04b">
-      <el-row v-for="(item,index) in navs" :key="index">
-      <el-submenu index="item.index" >
-        <template slot="title" >
-          <i class="el-icon-location"></i>
-          <span>{{item.text}}</span>
-        </template>    
-          <el-menu-item index="item1.index" 
-          v-for="(item1,index1) in item.children" :key="index1">
-            {{item1.text}}</el-menu-item>
-          <!-- <el-menu-item index="1-2">2&nbsp;&nbsp;广州&nbsp;&nbsp;粤港澳大湾区、泛珠江三角洲经济区</el-menu-item>
-          <el-menu-item index="1-3">3&nbsp;&nbsp;上海&nbsp;&nbsp;长江入海口，东隔东中国海</el-menu-item>
-          <el-menu-item index="1-4">4&nbsp;&nbsp;成都&nbsp;&nbsp;国家历史文化名城</el-menu-item>
-          <el-menu-item index="1-5">5&nbsp;&nbsp;西安&nbsp;&nbsp;中国国际形象最佳城市之一</el-menu-item> -->
-      </el-submenu>   
-      </el-row>  
-    </el-menu>
-    <div class="img">
-      <p>推荐城市</p><hr/>
-      <nuxt-link to='#'>
-        <img src="@/static/pic_sea.jpg" alt="">
-      </nuxt-link>
-    </div>
-  </el-col>
-</el-row>
-  </el-aside>
-  <el-container>
-    <el-header>
-      <div class="header">
+  <div class="main">
+        <div class="tab" @mouseleave="handleOver">
+              <div class="left">
+                     <el-row type='flex' class="item" justify='space-between' align='middle'
+                     v-for="(item,index) in navs" :key="index">
+                         <span @mouseenter="handleEnter(index)" :class="{active:current===index}">
+                             {{item.type}}<i class="el-icon-arrow-right"></i></span>
+                     </el-row>
+              </div>
+              <div class="right" v-if="current!==false">
+                  <ul>
+                      <li v-for="(item,index) in navs[current].children" :key="index">
+         {{index+1}}&nbsp;&nbsp;<Nuxt-link to='#'>{{item.city}}&nbsp;&nbsp;{{item.desc}}</Nuxt-link>
+                      </li>
+                  </ul>
+              </div>
+        </div>
+        <div class="img">
+            <h2>推荐城市</h2><hr>
+            <Nuxt-link to='#'>
+                <img src="@/static/pic_sea.jpg"  alt="">
+            </Nuxt-link>        
+        </div>
+    <div class="header">
         <el-row type='flex'>
-        <input type="text" :placeholder="item" v-model="item"><i class="el-icon-search"></i>   
+            <!-- <input type="text" placeholder="请输入想去的地方，比如：'广州'">
+            <i class="el-icon-search"></i> -->
+            <el-input placeholder="请输入想去的地方，比如：'广州'" v-model="city"
+            suffix-icon="el-icon-search"></el-input>
         </el-row>
-        <span>推荐: 
-          <!-- <a href="Javascript:;" ></a> -->
-          <nuxt-link to='#' 
-          v-for="(item,index) in ['广州','上海','北京']" :key="index"> 
-           {{item}}
-          </nuxt-link>      
+        <el-row class="city">
+            <span>推荐:&nbsp;&nbsp;
+            <nuxt-link v-for="(city,index) in ['广州','上海','北京']"
+             :key="index" :to="`/post/${city}`">{{city}}&nbsp;&nbsp;</nuxt-link>
         </span>
-        <el-row  type='flex' justify='space-between'>
-          <h2>推荐攻略</h2>
-          <el-button type='primary'>写游记</el-button>
         </el-row>
-      </div>
-    </el-header>
-    <el-main></el-main>
-  </el-container>
-</el-container>
-
+        <el-row type='flex' justify='space-between' class="title">
+            <h2>推荐攻略</h2>
+            <el-button type='primary'><i class="el-icon-edit"></i>写游记</el-button>
+        </el-row>      
+        <hr>
+    </div>
+    <div class="post">
+         <Postlist></Postlist>
+    </div>
+  </div>
 </template>
 
 <script>
+import Postlist from '@/components/post/postlist'
 export default {
-  data () {
-    return {
-          item:'',
-        
-          navs:[
-            {
-              index:1,
-              text:'热门城市',
-              children:[
-                {index:1-1,
-                 text:`1 北京 世界著名古都和现代化国际城市`},
-                {index:1-2,
-                 text:'2 广州 粤港澳大湾区、泛珠江三角洲经济区'},
-                {index:1-3,
-                 text:'3 上海 长江入海口，东隔东中国海'},
-                {index:1-4,
-                 text:'4 成都 国家历史文化名城'},
-                {index:1-5,
-                 text:'5 西安 中国国际形象最佳城市之一'}
-              ]
-            },
-            {
-              index:2,
-              text:'推荐城市',
-              children:[
-                {index:2-1,
-                 text:`1 青岛 滨海度假旅游城市`},
-                {index:2-2,
-                 text:'2 杭州 西湖十景'},
-                {index:2-3,
-                 text:'3 深圳 世界影响力的创新创意之都'},
-                {index:2-4,
-                 text:'4 广州 粤港澳大湾区、泛珠江三角洲经济区'},
-                {index:2-5,
-                 text:'5 成都 国家历史文化名城'}
-              ]
-            }
-          ]
-    }
-  },
-  methods: {
-      // handleOpen(key, keyPath) {
-      //   console.log(key, keyPath);
-      // },
-      // handleClose(key, keyPath) {
-      //   console.log(key, keyPath);
-      // }
-     
+    components:{
+        Postlist
+    },
+    data () {
+        return {
+            city:'',
+            current:false,
+            navs:[
+                // {
+                //     type:'热门城市',
+                //     children:[
+                //         {No:1,
+                //          city:'北京',
+                //          desc:' 世界著名古都和现代化国际城市'}, 
+                //         {No:2,
+                //          city:'广州',
+                //          desc:' 粤港澳大湾区、泛珠江三角洲经济区'}, 
+                //         {No:3,
+                //          city:'上海',
+                //          desc:'长江入海口，东隔东中国海'}, 
+                //         {No:4,
+                //          city:'成都',
+                //          desc:'国家历史文化名城'}, 
+                //         {No:5,
+                //          city:'西安',
+                //          desc:'中国国际形象最佳城市之一'},         
+                //     ]
+                // },
+                // {
+                //     type:'推荐城市',
+                //     children:[
+                //         {No:1,
+                //          city:'青岛',
+                //          desc:'  滨海度假旅游城市'}, 
+                //         {No:2,
+                //          city:'杭州',
+                //          desc:'  西湖十景'}, 
+                //         {No:3,
+                //          city:'深圳',
+                //          desc:'世界影响力的创新创意之都'}, 
+                //         {No:4,
+                //          city:'广州',
+                //          desc:'粤港澳大湾区、泛珠江三角洲经济区'}, 
+                //         {No:5,
+                //          city:'成都',
+                //          desc:'国家历史文化名城'},         
+                //     ]
+                // },
+                // {
+                //     type:'奔向海岛',
+                //     children:[
+                //         {No:1,
+                //          city:'秦皇岛',
+                //          desc:'   驰名中外的旅游休闲胜地'}, 
+                //         {No:2,
+                //          city:'青岛',
+                //          desc:'  滨海度假旅游城市'}, 
+                //         {No:3,
+                //          city:'珠海',
+                //          desc:'浪漫之城，百岛之市'}, 
+                //         {No:4,
+                //          city:'深圳',
+                //          desc:'世界影响力的创新创意之都'}, 
+                //         {No:5,
+                //          city:'海口',
+                //          desc:' 海滨自然旖旎风光'},         
+                //     ]
+                // },
+                // {
+                //     type:'主题推荐',
+                //     children:[
+                //         {No:1,
+                //          city:'海口',
+                //          desc:'   海滨自然旖旎风光'}, 
+                //         {No:2,
+                //          city:'广州',
+                //          desc:'  粤港澳大湾区、泛珠江三角洲经济区'}, 
+                //         {No:3,
+                //          city:'上海',
+                //          desc:' 长江入海口，东隔东中国海'}, 
+                //         {No:4,
+                //          city:'珠海',
+                //          desc:'浪漫之城，百岛之市'}, 
+                //         {No:5,
+                //          city:'西安',
+                //          desc:' 中国国际形象最佳城市之一'},         
+                //     ]
+                // },
+            ]
+        }
+    },
+
+    methods: {
+        handleEnter(index){
+            this.current=index;
+        },
+        handleOver(){
+            this.current=false;
+        }
+    },
+
+    mounted () {
+        this.$axios({
+            url:'/posts/cities'
+        }).then(res=>{
+            console.log(res);
+            this.navs=res.data.data;
+        })
+
+
+        this.$axios({
+            url:'/posts',
+            city:this.city
+        }).then(res=>{
+            console.log(res);
+            
+        })
     }
 
 }
 </script>
 
-<style>
-.img p{
-  padding-top:20px;
-  padding-bottom:20px;
+<style scoped lang='less'>
+.main{
+    margin:0 auto;
+    width:1200px;
+    position: relative;
+    
 }
-.img img{
-  width:100%;
+.tab{
+    margin-top: 30px;
+    position:relative;
 }
-.header input{
-    display: flex;
-    flex:1;
-    height:40px;
+.left{
+    width:300px;
+   
+    position: relative;
+    top:0;
+    left: 0;
+    z-index: 9;
+}
+.item{
+    width:300px;
+    height:50px;
+    border:1px #ccc solid;
+
+    /* span .active{
+        padding-left: 20px;
+    } */
+}
+ .item  .active{
+        border-right:none;       
+    }
+  
+.right{
+    width:350px;
+    height:200px;
+    border:1px #ccc solid;
+    position:absolute;  
+    top: 0;
+    left:300px;
+    background-color: #ccc;
+
+    /* li {
+        margin:25px;
+    } */
+}
+.right li {
+        padding:10px;
+    }
+
+    .img{
+        margin:20px 0;
+        width:300px;      
+    }
+
+  .img  h2 {
+            margin:20px 0;
+        }
+
+ .img img{
+           width:100%; 
+           margin:20px 0;  
+        }
+
+  .header{
+      position:absolute;
+      top:0;
+      left:400px;  
+      width:800px;
+      
+  }
+  
+
+//   .header input{
+//       display: flex;
+//       flex:1;
+//       height:50px;
+//       border:2px solid orange;
+//   }
+.el-input  /deep/ input{
+    
+    height:50px;
     border:2px solid orange;
 }
-.el-submenu span:hover{
-         color:yellow;
-}
-  /* .el-header, .el-footer {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
-  } */
-  
-  .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-    line-height: 200px;
+
+  .city{
+      margin-top: 10px;
+      margin-bottom: 15px;
   }
-  
-  /* .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: center;
-    line-height: 160px;
-  } */
-  
-  body > .el-container {
-    margin-bottom: 40px;
+  .title {
+      margin:10px auto;
+      h2{
+          color:orange;
+      }
   }
-  
-  .el-container:nth-child(5) .el-aside,
-  .el-container:nth-child(6) .el-aside {
-    line-height: 260px;
-  }
-  
-  .el-container:nth-child(7) .el-aside {
-    line-height: 320px;
+  .post{
+       position: absolute;
+       top:200px;
+       left:400px;
   }
 </style>
